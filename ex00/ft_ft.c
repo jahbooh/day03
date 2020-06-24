ff6442653366a58259fdd6a0518c662cd608fbd6
Jabulile Simelane <jasimela@c5r6s7.wethinkcode.co.za>
2020-06-24 15:35:53 +0200

    ex00

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,8 +6,37 @@
 /*   By: jasimela <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 17:01:02 by jasimela          #+#    #+#             */
-/*   Updated: 2020/06/19 17:06:19 by jasimela         ###   ########.fr       */
+/*   Updated: 2020/06/24 15:35:24 by jasimela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<unistd.h>
+#include<stdio.h>
 
+void  ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void  ft_putint(int i)
+{
+  printf("%d",i);
+}
+
+void  ft_ft(int *nbr)
+{
+  int i;
+  
+  i = 42;
+  *nbr = i;
+}
+
+/*int main(void)
+{
+  int c;
+  
+  c = 0;
+  ft_ft(&c);
+  ft_putint(c);
+  return (0);
+}\*
